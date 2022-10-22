dda3974abb5ec837b692c7ce0a11360b5c843954
rishikesh.s <rdshinde2000@gmail.com>
2022-10-22 22:09:56 +0530

    config: tailwind css config

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
   return (
@@ -13,14 +13,16 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-       <h1>Rishikesh Shinde</h1>
+        <h1 className="text-gray-100 text-3xl">Rishikesh Shinde</h1>
       </main>
 
       <footer className={styles.footer}>
-       <small>Built with ❤️ by Rishikesh Shinde</small>
+        <small className="text-gray-500">
+          Built with ❤️ by Rishikesh Shinde
+        </small>
       </footer>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
